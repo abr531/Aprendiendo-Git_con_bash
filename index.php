7d819f0748ef8a5b207a47e1ba126c7ddf043f38
Abraham <abraham.retamal777@gmail.com>
2024-05-16 12:46:52 -0400

    Segundo Commit con nuevos cambios

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ $apellido = 'Retamal';
 <body>
     <h1 class="text-center mt-3">prueba php en codigo</h1>
     <h4>Bienvenido <?php echo " $nombre $apellido " ?> </h4>
+    <h4>Bienvenido <?php echo " $nombre $apellido " ?> </h4>
+    <h4>Bienvenido <?php echo " $nombre $apellido " ?> </h4>
+    <h4>Bienvenido <?php echo " $nombre $apellido " ?> </h4>s
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
